2b4814257a22d9b4253e170a63934413194cd38c
Thr1ve <fict1on.buhler@gmail.com>
2015-07-04 03:04:13 -0400

    initial

---
+++ index.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import createSideEffect from 'react-side-effect'
+
+let _keyMap = {};
+
+let Keybindings = createSideEffect(function handleChange(propsList) {
+
+  let keyMap = {};
+
+  propsList.forEach(function (props) {
+    Object.assign(keyMap, props.keyMap);
+  });
+
+  for (var key in keyMap) {
+    Mousetrap.bind(key, keyMap[key])
+    _keyMap[key] = keyMap[key];
+  }
+},{
+  componentWillUnmount() {
+    for (var key in _keyMap){
+      Mousetrap.unbind(key)
+    }
+  }
+});
+
+module.exports = Keybindings;
